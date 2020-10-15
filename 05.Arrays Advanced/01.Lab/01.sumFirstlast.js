c01627c42e14e318ef897f0427884510d02e532a
Georgi Ivanov <subs@gr-ivanov.com>
2020-10-15 19:16:33 +0300

    first try

---
+++ 05.Arrays Advanced/01.Lab/01.sumFirstlast.js
@@ -0,0 +1,9 @@
+function sumFirstLast(array) {
+    let firstElement = array[0];
+    let lastElement = array.pop;
+
+    return firstElement + lastElement;
+
+}
+
+sumFirstLast(['20', '30', '40']);
