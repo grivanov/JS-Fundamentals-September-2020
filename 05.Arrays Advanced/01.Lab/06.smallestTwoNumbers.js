1be403aa3235c2c4f67bcca2a656a97abfdf137b
Georgi Ivanov <subs@gr-ivanov.com>
2020-10-16 16:17:04 +0300

    second way to solve

--- 05.Arrays Advanced/01.Lab/06.smallestTwoNumbers.js
+++ 05.Arrays Advanced/01.Lab/06.smallestTwoNumbers.js
@@ -1,29 +1,11 @@
 function smallestTwoNumbers(array) {
-    let modifiedArray = [];
-    let len = array.length;
-    let firstSmallest = array[0];
+    let result = [];
+    array.sort((a, b) => a - b);
+    result = array.slice(0,2);
 
-    for (let i = 1; i < len; i++) {
-        if (array[i] < firstSmallest) {
-            modifiedArray.push(firstSmallest);
-            firstSmallest = array[i];
-        } else {
-            modifiedArray.push(array[i]);
-        }
-    }
-
-    let secondSmallest = modifiedArray[0];
-    let lenModified = modifiedArray.length;
-
-    for (let i = 1; i < lenModified; i++) {
-        if (modifiedArray[i] < secondSmallest) {
-            secondSmallest = modifiedArray[i];
-        }
-    }
-
-    console.log(`${firstSmallest} ${secondSmallest}`);
+    console.log(result.join(' '));
 }
 
 
-smallestTwoNumbers([3, 0, 10, 4, 7, 3]);
+smallestTwoNumbers([30, 15, 50, 5]);
 
