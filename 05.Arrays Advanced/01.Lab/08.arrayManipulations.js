1e13feee2452a6007053c4ad5c1d7fd17b72de84
Georgi Ivanov <subs@gr-ivanov.com>
2020-10-16 17:50:06 +0300

    proper solution

--- 05.Arrays Advanced/01.Lab/08.arrayManipulations.js
+++ 05.Arrays Advanced/01.Lab/08.arrayManipulations.js
@@ -1,30 +1,27 @@
 function arrayManipulations(array) {
-    let workingArray = array.shift().split(' ');
+    let workingArray = array.shift().split(' ').map(Number);
 
-    for (let el of array) {
-        if (el.includes('Add')) {
-            let tempArray = el.split('');
-            let number = tempArray.pop();
-            workingArray.push(number);
-        } else if (el.includes('RemoveAt')) {
-            let tempArray = el.split('');
-            let number = tempArray.pop();
-            workingArray.splice(number, 1);
-        } else if (el.includes('Remove')) {
-            let tempArray = el.split('');
-            let number = tempArray.pop();
-            let index = Number(workingArray.filter((x, i) => x == number));
-            workingArray.splice(index, 1);
-        } else if (el.includes('Insert')) {
-            let tempArray = el.split(' ');
-            let index = tempArray.pop();
-            let number = tempArray.pop();
-            workingArray.splice(index, 0, number)
+    for (let i = 0; i < array.length; i++) {
+        let [command, firstNum, secondNum] = array[i].split(' ');
+        firstNum = Number(firstNum);
+        secondNum = Number(secondNum);
+
+        switch (command) {
+            case 'Add':
+                workingArray.push(firstNum);
+                break;
+            case 'Remove':
+                workingArray = workingArray.filter(el => el !== firstNum);
+                break;
+            case 'RemoveAt':
+                workingArray.splice(firstNum, 1);
+                break;
+            case 'Insert':
+                workingArray.splice(secondNum, 0, firstNum);
+                break;
         }
     }
-
     console.log(workingArray.join(' '));
-
 }
 
 arrayManipulations(['4 19 2 53 6 43', 'Add 3', 'Remove 2', 'RemoveAt 1', 'Insert 8 3']);
