77475c51919f7594b7fd04fa80d347217e6b916d
Georgi Ivanov <subs@gr-ivanov.com>
2020-10-19 20:35:41 +0300

    trying stuff

--- 05.Arrays Advanced/02.Exercises/05.sortArrayTwoCriterias.js
+++ 05.Arrays Advanced/02.Exercises/05.sortArrayTwoCriterias.js
@@ -1,21 +1,27 @@
 function sortArrayTwoCriterias(inputArray) {
     let sortedArray = inputArray.slice();
+    let equalElements = [];
 
-    function sorting(a, b) {
-        if (a.length < b.length) {
-            return -1;
+
+    for (let i = 0; i < sortedArray.length; i++) {
+        if (sortedArray[i].length == sortedArray[i + 1].length) {
+            equalElements.push(sortedArray[i], sortedArray[i + 1]);
+            let index = equalElements.indexOf(equalElements[i], 0)
+            equalElements.splice(index, 1);
+            equalElements.sort((a, b) => a.localeCompare(b));
         } else {
-            return 1;
+            equalElements.push(sortedArray[i + 1]);
+            let index = equalElements.indexOf(equalElements[i], 0)
+            equalElements.splice(index, 1);
         }
+        console.log(equalElements);
     }
 
-    sortedArray.sort(sorting);
-    
-    for (let element of sortedArray) {
-        console.log(element);
-    }
+
+    console.log(sortedArray.join(' '));
+
 }
 
-sortArrayTwoCriterias(["alpha", "beta", "gamma"]);
-sortArrayTwoCriterias(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
+// sortArrayTwoCriterias(["alpha", "beta", "gamma"]);
+// sortArrayTwoCriterias(["Isacc", "Theodor", "Jack", "Harrison", "George"]);
 sortArrayTwoCriterias(["test", "Deny", "omen", "Default"]);
