f7267d4043d1d9dbc9009b34ca812f834165044e
Georgi Ivanov <subs@gr-ivanov.com>
2020-11-14 22:55:42 +0200

    first try

---
+++ 06.Objects-Classes/01.Lab/03.convertToObject.js
@@ -0,0 +1,13 @@
+function convert(inputJSON) {
+    let person = JSON.parse(inputJSON);
+
+    for (let [key, value] of Object.entries(person)) {
+        console.log(`${key}: ${value}`);
+
+    }
+
+}
+
+let input = '{"name": "George", "age": 40, "town": "Sofia"}';
+
+convert(input);
