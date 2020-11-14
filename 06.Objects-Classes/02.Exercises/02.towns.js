600c86b22235540c7cfb9201ed4162626bfe46c8
Georgi Ivanov <subs@gr-ivanov.com>
2020-11-15 01:20:27 +0200

    solve without a class

--- 06.Objects-Classes/02.Exercises/02.towns.js
+++ 06.Objects-Classes/02.Exercises/02.towns.js
@@ -1,20 +1,24 @@
 function towns(inputArray) {
 
-    class Town {
-        constructor(name, latt, long) {
-            this.town = name;
-            this.latitude = Number(latt).toFixed(2);
-            this.longitude = Number(long).toFixed(2);
-        }
-
-    }
+    // class Town {
+    //     constructor(name, latt, long) {
+    //         this.town = name;
+    //         this.latitude = Number(latt).toFixed(2);
+    //         this.longitude = Number(long).toFixed(2);
+    //     }
+    // }
 
     for (let el of inputArray) {
         let [name, latt, long] = el.split(' | ');
-        let newTown = new Town(name, latt, long);
-        return newTown;
+        latt = Number(latt).toFixed(2);
+        long = Number(long).toFixed(2);
+        let entry = {
+            town: name,
+            latitude: latt,
+            longitude: long
+        }
+        console.log(entry);
     }
-
 }
 
 towns(['Sofia | 42.696552 | 23.32601', 'Beijing | 39.913818 | 116.363625']);
