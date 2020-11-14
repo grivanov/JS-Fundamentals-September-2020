ebd8f715c8abdd6d1040823605ecb005094300d9
Georgi Ivanov <subs@gr-ivanov.com>
2020-11-15 00:14:39 +0200

    first try

---
+++ 06.Objects-Classes/02.Exercises/01.employees.js
@@ -0,0 +1,26 @@
+function employees(inputArray) {
+
+        class Employee {
+            constructor(name) {
+                this.name = name;
+                this.number = name.length;
+            }
+
+            printCard() {
+                console.log(`Name: ${this.name} -- Personal Number: ${this.number}`)
+            }
+        }
+
+        for (let el of inputArray) {
+            let newEmployee = new Employee(el);
+            newEmployee.printCard();
+        }
+}
+
+employees([
+    'Silas Butler',
+    'Adnaan Buckley',
+    'Juan Peterson',
+    'Brendan Villarreal'
+    ]
+    );
